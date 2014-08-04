29b4d524b72979ccd24a4387e57fe36d96a60a65
ffMathy <mathias.lorenzen@live.com>
2014-08-04 16:06:47 +0200

    initial commit.

---
+++ WindowControl/WindowControl.Sample/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Controls.Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : WindowControl
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WindowControl/WindowControl/WindowControl.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+using System.Windows.Media;
+using AeroColor;
+
+namespace Controls
+{
+    [ContentProperty("Content")]
+    public class WindowControl : Window
+    {
+        private readonly Border _border;
+
+        public WindowControl()
+        {
+            ResizeMode = ResizeMode.NoResize;
+            WindowStyle = WindowStyle.None;
+
+            //create a fake border.
+            var border = new Border();
+            border.BorderThickness = new Thickness(2);
+
+            base.Content = border;
+
+            _border = border;
+
+            Loaded += WindowControl_Loaded;
+        }
+
+        void WindowControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            AeroResourceInitializer.Initialize();
+            _border.SetResourceReference(Border.BorderBrushProperty, "AeroBrushDark");
+        }
+
+        public new object Content
+        {
+            get { return _border.Child; }
+            set { _border.Child = value as UIElement; }
+        }
+
+        public new static DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof (object),
+            typeof (WindowControl), new PropertyMetadata(null));
+
+    }
+}
